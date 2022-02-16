2eb0e5f74c2498d6cf3ed4f64fab29f21fcdb35f
longle <longle@mgtv.com>
2022-02-16 15:15:26 +0800

    ball game

---
+++ ball-game-site/scripts/main.js
@@ -0,0 +1,98 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+console.log('width:' + width + ' height:' + height);
+
+let balls = [];
+
+while (balls.length < 35) {
+    let size = random(10, 20);
+    let ball = new Ball(random(0 + size, width - size),
+        random(0 + size, height - size),
+        random(-7, 7),
+        random(-7, 7),
+        randomColor(),
+        size);
+    balls.push(ball);
+}
+
+loop();
+
+function loop() {
+    ctx.fillStyle = 'rgba(0,0,0, 0.25)';
+    ctx.fillRect(0, 0, width, height);
+
+    for (let i = 0; i < balls.length; i++) {
+        balls[i].draw();
+        balls[i].update();
+        balls[i].collisionDetect();
+        console.log('i: ' + i);
+    }
+
+    requestAnimationFrame(loop);
+}
+
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function randomColor() {
+    return 'rgb(' +
+        random(0, 255) + ', ' +
+        random(0, 255) + ', ' +
+        random(0, 255) + ')';
+}
+
+function Ball(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+
+    Ball.prototype.draw = function() {
+        console.log('draw:' + this.color);
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+
+    Ball.prototype.update = function() {
+        if ((this.x + this.size) >= width) {
+            this.velX = -(this.velX);
+        }
+
+        if ((this.x - this.size) <= 0) {
+            this.velX = -(this.velX);
+        }
+
+        if ((this.y + this.size) >= height) {
+            this.velY = -(this.velY);
+        }
+
+        if ((this.y - this.size) <= 0) {
+            this.velY = -(this.velY);
+        }
+
+        this.x += this.velX;
+        this.y += this.velY;
+    }
+
+    Ball.prototype.collisionDetect = function() {
+        for (let j = 0; j < balls.length; j++) {
+            if (this !== balls[j]) {
+                const dx = this.x - balls[j].x;
+                const dy = this.y - balls[j].y;
+                const distance = Math.sqrt(dx * dx + dy * dy);
+
+                if (distance < this.size + balls[j].size) {
+                    balls[j].color = this.color = randomColor();
+                }
+            }
+        }
+    }
+}
